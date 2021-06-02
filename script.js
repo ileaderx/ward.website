e30767ccfed00d690342a8c7556ae83c78ba5489
WARD <35292306+ileaderx@users.noreply.github.com>
2021-06-02 17:53:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,24 @@ $(document).ready(function(){
       $(".backToTop").hide();
     }
   });
+  
+  $("nav ul li a[href^='#']").on('click', function(e) {
+
+    // prevent default anchor click behavior
+    e.preventDefault();
+ 
+    // store hash
+    var hash = this.hash;
+ 
+    // animate
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 700, function(){
+ 
+        // when done, add hash to url
+        // (default click behaviour)
+        window.location.hash = hash;
+      });
+ 
+ });
+  
